0952a56bc5b7d08f16c1b04dab70cddb2afce5a5
이영민 <fkvkxk1@naver.com>
2024-11-15 19:26:23 +0900

    Initial Commit

---
+++ Swift_Study/Swift_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_StudyApp.swift
+//  Swift_Study
+//
+//  Created by 이영민 on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift_StudyTests/Swift_StudyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_StudyTests.swift
+//  Swift_StudyTests
+//
+//  Created by 이영민 on 11/15/24.
+//
+
+import Testing
+@testable import Swift_Study
+
+struct Swift_StudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
